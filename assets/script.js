50963af4faddecac9c2793ce02fa174f26f503ec
DrDano <danielharned@gmail.com>
2021-12-14 16:01:07 -0800

    adding comment posting functionality along with a new metric

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 var submitUrl = document.querySelector("#submit-url");
-var urlInput = document.querySelector("#url-input")
-var chartCon = document.querySelector("#comments-likes-chart")
+var urlInput = document.querySelector("#url-input");
+var chartCon = document.querySelector("#comments-likes-chart");
+var commentCon = document.querySelector("#comments-container");
+var totalCommentLikes = 0;
 
 // Make sure the client is loaded before calling this method.
 function getComments(videoID) {
@@ -9,6 +11,7 @@ function getComments(videoID) {
       response.json()
       .then(function(data) {
         console.log(data)
+        displayComments(data)
       })
     } else {
       console.log("response not ok")
@@ -16,11 +19,24 @@ function getComments(videoID) {
   })
 }
 
+function displayComments(commentData) {
+  var commentArr = commentData.items
+  for (var i = 0; i < commentArr.length; i++) {
+    var newLi = document.createElement("li");
+    newLi.id = `comment-${i}`;
+    newLi.innerText = commentArr[i].snippet.topLevelComment.snippet.textOriginal;
+    totalCommentLikes += parseInt(commentArr[i].snippet.topLevelComment.snippet.likeCount);
+    commentCon.appendChild(newLi);
+  }
+  getVideoStats(urlInput.value.split('=')[1]);
+}
+
 function getVideoStats(videoID) {
   fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet%2Cstatistics&id=${videoID}&key=AIzaSyBQM1YK0xl9l3Bvy2OntNplW7gTx9RZkGs`).then(function(response) {
     if (response.ok) {
       response.json()
       .then(function(data) {
+        console.log(data)
         getChart(data)
       })
     } else {
@@ -30,7 +46,8 @@ function getVideoStats(videoID) {
 }
 
 function getChart(videoData) {
-
+  var comments = videoData.items[0].statistics.likeCount
+  var likes = videoData.items[0].statistics.commentCount
   fetch("https://quickchart.io/chart/create", {
     headers: {
       "Content-Type": "application/json"
@@ -41,7 +58,7 @@ function getChart(videoData) {
       "width": 500,
       "height": 300,
       "format": "png",
-      "chart": `{type:'doughnut',data:{labels:['Comments','Likes','Views'],datasets:[{data:[120,30000,800000]}]},options:{plugins:{doughnutlabel:{labels:[{text:'550',font:{size:20}},{text:'total'}]}}}}`
+      "chart": `{type:'doughnut',data:{labels:['Comments','Likes', 'Likes on Comment Threads'],datasets:[{data:[${likes},${comments},${totalCommentLikes}]}]},options:{plugins:{doughnutlabel:{labels:[{text:'${parseInt(comments) + parseInt(likes) + parseInt(totalCommentLikes)}',font:{size:20}},{text:'total'}]}}}}`
     })
   }).then(function(response) {
     if(response.ok) {
@@ -56,14 +73,13 @@ function getChart(videoData) {
 function displayChart(chartUrl) {
   var newImg = document.createElement("img");
   newImg.id = "chart-image";
-  console.log(chartUrl)
   newImg.setAttribute("src", chartUrl);
   chartCon.appendChild(newImg);
 }
 
 submitUrl.addEventListener("click", function getVideoID(event) {
   event.preventDefault();
-  console.log(urlInput.value.split('=')[1])
   // getVideoStats(urlInput.value.split('=')[1])
-  getChart("data")});
+  getComments(urlInput.value.split('=')[1]);
+});
 
